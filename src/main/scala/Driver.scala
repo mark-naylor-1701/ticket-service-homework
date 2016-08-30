2269386a31354341b6f540b72f29bcf5b7079bbb
Mark W. Naylor <mark.naylor.1701@gmail.com>
2016-08-30 13:06:21 -0400

    Update Driver test harness

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -4,36 +4,92 @@
 
 import scala.collection.JavaConverters._
 
-import java.util.{ArrayList}
-import java.util.Optional
+import java.util.{ArrayList, Collection, Date, Optional}
 
 import com.walmart.homework._
 
-object Data {
-  val venue = Venue.defaultVenue
-  val testVenue = Venue.testingVenue
-
-  val none = Optional.empty()
-  val noneInt: Optional[java.lang.Integer] = Optional.empty()
-
-  val email = "mark.naylor.1701@gmail.com"
-
-  def someInt(i: java.lang.Integer): Optional[java.lang.Integer] = Optional.of(i)
-}
-
 object Driver extends App {
   import Data._
+  import Functions.{printLabel, someInt}
 
   println("Hello, Walmart testing world.")
 
-  test1
+  printLabel("testCreateLevel")
+  testCreateLevel
+
+  printLabel("testCreateSeat")
+  testCreateSeat
+
+  printLabel("testCreateSeatHold")
+  testCreateSeatHold
+
+  printLabel("testCreateVenue")
+  testCreateVenue
+
 
   // -- Support functions
   def showSeats(venue: Venue) = println("Available seats: " + venue.numSeatsAvailable(noneInt))
 
   // -- Test funtions
 
+  def testCreateSeat() {
+    // Seat depends on having a good level.
+    testCreateLevel
+
+    val level = new Level(1, new LevelName("Test Name"), new MonetaryAmount(15.50), 4, 10)
+    val rowNumber = new RowNumber(1)
+    val seatNumber = new SeatNumber(2)
+
+    val seat = new Seat(rowNumber, seatNumber, level)
+    assert(seat != null, "No seat created")
+
+
+    assert(seat.getLevel == level, s"level ${seat.getLevel} != ${level}")
+    assert(seat.getRowNumber == rowNumber, s"Field ${seat.getRowNumber} != ${rowNumber}")
+    assert(seat.getSeatNumber == seatNumber, s"Field ${seat.getSeatNumber} != ${seatNumber}")
+  }
+
+  def testCreateLevel() {
+    val name = new LevelName("Test Name")
+    val price = new MonetaryAmount(15.50)
+    val rows = 4
+    val seats = 10
+    val id = 1
+    val level = new Level(id, name, price, rows, seats)
+
+    assert(level.getName == name, s"Field ${level.getName} != ${name}")
+    assert(level.getPrice == price, s"Field ${level.getPrice} != ${price}")
+    assert(level.getId == id, s"Field ${level.getId} != ${id}")
+    assert(level.numSeatsAvailable == rows * seats, s"Field ${level.numSeatsAvailable} != ${rows * seats}")
+  }
+
+  def testCreateVenue() {
+    val tv = Venue.testingVenue
+    assert(tv != null, s"No Venue returned")
+
+    val initialSeats = tv.numSeatsAvailable(noneInt)
+    assert(initialSeats == 40, s"initial seats ${initialSeats} != 40")
+  }
+
+  def testCreateSeatHold() {
+    val none: Optional[Collection[Seat]] = Optional.empty()
+    val sh = new SeatHold(email, none)
+
+    //println(sh.getExpiration)
+    val now = new Date
+    val expiration = sh.getExpiration
+    assert(expiration > now.getTime, s"Field ${expiration} <= ${now}")
+
+    val size = sh.getSeats.size
+    assert(size == 0, s"Failure on SeatHold getSeats, ${size} != 0\n")
+
+    val resEmail = sh.getCustomerEmail
+    assert(resEmail == email, s"Failure on SeatHold getCustomerEmail, ${resEmail} != ${email}\n")
+
+  }
+
   def test1() {
+    val testVenue = Venue.testingVenue
     showSeats(testVenue)
 
     println("\n Any seats: " + testVenue.findAndHoldSeats(15, noneInt, noneInt, email))
@@ -66,7 +122,25 @@ object Driver extends App {
     println
 
     showSeats(testVenue)
-    
+
   } // test1()
 
-}
+} // Driver
+
+
+
+
+object Data {
+
+  val none = Optional.empty()
+  val noneInt: Optional[java.lang.Integer] = Optional.empty()
+
+  val email = "mark.naylor.1701@gmail.com"
+
+} // Data
+
+object Functions {
+  def someInt(i: java.lang.Integer): Optional[java.lang.Integer] = Optional.of(i)
+
+  def printLabel(label: String) = println(s"\n${label}")
+} // Functions
