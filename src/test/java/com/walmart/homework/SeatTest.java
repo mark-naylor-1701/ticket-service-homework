0b83b6f9c1454c3f337ae57072ab53a0272b39a5
Mark W. Naylor <mark.naylor.1701@gmail.com>
2016-09-03 17:37:23 -0400

    Add test class for Seat

---
+++ src/test/java/com/walmart/homework/SeatTest.java
@@ -0,0 +1,17 @@
+package com.walmart.homework;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SeatTest {
+
+    @Test
+    public final void testSeat() {
+        RowNumber rowNumber = new RowNumber(1);
+        SeatNumber seatNumber = new SeatNumber(2);
+        Seat seat = new Seat(rowNumber, seatNumber, null);
+        assertNotNull("No seat created", seat );
+    }
+
+}
